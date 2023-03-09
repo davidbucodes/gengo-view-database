ed1b7a9d47bd07fc079eb6be83a5e1274f1baf52
david <david@david.david>
2023-03-10 00:33:03 +0200

    feat: update romaji regexp

--- src/regexp/isValidRomajiRegexp.ts
+++ src/regexp/isValidRomajiRegexp.ts
@@ -1,2 +1,2 @@
 export const isValidRomajiRegexp =
-  /^(a|i|u|e|o|n|fu|\s|([kstnhmyrwngzdbp]|gy|ky|kk|kky|sh|ss|ssh|ch|cc|cch|ny|hy|by|my|py|pp|ppy|ry|tt)[aiueo])*$/i;
+  /^(a|i|u|e|o|n|m|fu|tsu|dzu|\s|([kstnhmyrwngzdbpj]|gy|ky|kk|kky|sh|ss|ssh|ch|cc|cch|ny|hy|by|my|py|pp|ppy|ry|tt|jy)[aiueo])*$/i;
